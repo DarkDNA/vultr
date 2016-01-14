372c475b8ad0eb4116afcfaf1829b01231020464
Amanda Cameron <amanda@darkdna.net>
2016-01-14 14:43:01 -0500

    Fix SSH Key stuff, hasty commits are bad, mmkay?

--- ssh_keys.go
+++ ssh_keys.go
@@ -11,23 +11,28 @@ func (cli *Client) DestroySSHKey(keyID string) error {
 }
 
 // UpdateSSHKey updates an existing SSH Key.
-func (cli *Client) UpdateSSHKey(keyID, name, sshKey string) (string, error) {
+func (cli *Client) UpdateSSHKey(keyID, name, sshKey string) error {
 	params := map[string]string{
 		"name":     name,
 		"ssh_key":  sshKey,
 		"SSHKEYID": keyID,
 	}
 
-	resp, err := cli.RequestMap(params, "/sshkey/update", "POST")
+	_, err := cli.RequestMap(params, "/sshkey/update", "POST")
+	return err
 }
 
 // CreateSSHKey creates a new SSH Key for sending to hosts.
-func (cli *Client) CreateSSHKey(name, sshKey string) error {
+func (cli *Client) CreateSSHKey(name, sshKey string) (string, error) {
 	params := map[string]string{
 		"name":    name,
 		"ssh_key": sshKey,
 	}
 
-	_, err := cli.RequestMap(params, "/sshkey/create", "POST")
-	return err
+	resp, err := cli.RequestMap(params, "/sshkey/create", "POST")
+	if err != nil {
+		return "", err
+	}
+
+	return resp["SSHKEYID"].(string), nil
 }
